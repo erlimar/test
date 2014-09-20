0fcbdb8070b3d35a84bb90aa92412e73a374beec
Erlimar Silva Campos <erlimar@gmail.com>
2014-09-20 13:25:50 -0300

    Initial commit

---
+++ TestNugetRestorePackages/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestNugetRestorePackages.Startup))]
+namespace TestNugetRestorePackages
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
